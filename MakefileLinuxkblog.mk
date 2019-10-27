5bfc9bf5a86d357153787e1477057ce9bd18dcf2
kevinluolog <kevinluolog_72@163.com>
2019-10-27 22:06:49 +0800

    test03

--- MakefileLinuxkblog.mk
+++ MakefileLinuxkblog.mk
@@ -20,21 +20,18 @@ SRC_PATH_FILES := $(foreach dir,$(SRC_PATH_DIR),$(wildcard $(dir)/*.md))
 
 ###---生成目标
 
-tv := 0
-
 define PROGRAM_template
 #这个touch的机制可以工作，从git log 获得最后一次commit的时间。但是由于push up上去时其实是不带文件时间的。所以没用，要在hexo g，时改动才会在hexo的静态网站中体现出来。
 #同时注意在这个地方要用两个$$,函数和变量都是。只用一个会永远是最后一个文件的时间，因为第一次展开就被执行了。
-tv =: $(tv)+1
-all_time_$(tv) := $$(shell git log --date=iso --format="%ad" -- "$(1)")
-touch_time_$(tv) := $$(shell tail -1 $$(all_time_$(tv)))
+all_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)")
+touch_time_$(1) := $$(shell tail -1 $$(all_time_$(1)))
 $(1): 
-	@echo "all_time_$(tv)="
-	@echo "$$(all_time_$(tv))"
-	@echo "touch_time_$(tv)= $$(touch_time_$(tv))"
+	@echo "all_time_$(1)="
+	@echo "$$(all_time_$(1))"
+	@echo "touch_time_$(1)= $$(touch_time_$(1))"
 #	touch --date="" -m $filename
-#	touch --date="$$(touch_time_$(tv))" -m $$@
-	touch --date="$$(touch_time_$(tv))" $$@
+#	touch --date="$$(touch_time_$(1))" -m $$@
+	touch --date="$$(touch_time_$(1))" $$@
 
 	@echo "touch1 ok! $@"
 endef
