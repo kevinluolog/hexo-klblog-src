92ca697c50a58f29100b62ba5b92654428d4a745
kevinluolog <kevinluolog_72@163.com>
2019-10-28 11:21:07 +0800

    t21

--- MakefileLinuxkblog.mk
+++ MakefileLinuxkblog.mk
@@ -50,7 +50,7 @@ $(foreach temp,$(SRC_PATH_FILES),$(eval $(call PROGRAM_template,$(temp))))
 ###---伪目标
 .phony: $(SRC_PATH_FILES) touch1
 
-touch1: $(SRC_PATH_FILES)
+touch1:
 	echo $@
 	@echo all files is touch back first commit time.
 	@echo $(DIR_BASE_SRC)
