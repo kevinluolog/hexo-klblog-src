08b21325d4f26e19b39da41d905322573dc2622d
kevinluolog <kevinluolog_72@163.com>
2019-10-28 09:26:53 +0800

    test11

--- MakefileLinuxkblog.mk
+++ MakefileLinuxkblog.mk
@@ -23,13 +23,14 @@ SRC_PATH_FILES := $(foreach dir,$(SRC_PATH_DIR),$(wildcard $(dir)/*.md))
 define PROGRAM_template
 #这个touch的机制可以工作，从git log 获得最后一次commit的时间。但是由于push up上去时其实是不带文件时间的。所以没用，要在hexo g，时改动才会在hexo的静态网站中体现出来。
 #同时注意在这个地方要用两个$$,函数和变量都是。只用一个会永远是最后一个文件的时间，因为第一次展开就被执行了。
+#all_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)")
+#touch_time_$(1) := $$(shell tail -1 $$(all_time_$(1)))
+
 all_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)")
-touch_time_$(1) := $$(shell tail -1 $$(all_time_$(1)))
+touch_time_$(1) := $$(shell 'git log --date=iso --format="%ad" -- "$(1)" | tail -1') 
 $$(info $$(all_time_$(1)))
 $$(info $$(touch_time_$(1)))
 $$(info $(1))
-#all_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)")
-#touch_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)" | tail -1) 
 $(1): 
 	@echo "touch1 ok! $$@"
 #	@echo "all_time_$(1)="
