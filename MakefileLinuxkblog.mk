fdc691d29fabe5848b0800a44357492e8af29134
kevinluolog <kevinluolog_72@163.com>
2019-10-28 08:59:19 +0800

    test09

--- MakefileLinuxkblog.mk
+++ MakefileLinuxkblog.mk
@@ -25,17 +25,20 @@ define PROGRAM_template
 #同时注意在这个地方要用两个$$,函数和变量都是。只用一个会永远是最后一个文件的时间，因为第一次展开就被执行了。
 #all_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)")
 #touch_time_$(1) := $$(shell tail -1 $$(all_time_$(1)))
-all_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)")
-touch_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)" | tail -1) 
+$(info touch_time_$(1))
+$(info $(1))
+#all_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)")
+#touch_time_$(1) := $$(shell git log --date=iso --format="%ad" -- "$(1)" | tail -1) 
 $(1): 
-	@echo "all_time_$(1)="
-	@echo "$$(all_time_$(1))"
-	@echo "touch_time_$(1)= $$(touch_time_$(1))"
-#	touch --date="" -m $filename
-#	touch --date="$$(touch_time_$(1))" -m $$@
-	touch --date="$$(touch_time_$(1))" $$@
-
-	@echo "touch1 ok! $@"
+	@echo "touch1 ok! $$@"
+#	@echo "all_time_$(1)="
+#	@echo "$$(all_time_$(1))"
+#	@echo "touch_time_$(1)= $$(touch_time_$(1))"
+##	touch --date="" -m $filename
+##	touch --date="$$(touch_time_$(1))" -m $$@
+##	touch --date="$$(touch_time_$(1))" $$@
+#
+#	@echo "touch1 ok! $$@"
 endef
 
 # 打散目标集合,一个一个送入命令集重组,同时用eval命令在makefile中使能。这样可以克服模式匹配依赖要一致的缺点(%只能匹配文件名,并且要规则一样)
