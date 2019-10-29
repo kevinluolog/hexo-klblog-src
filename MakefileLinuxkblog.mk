d506d68c5c82b9e7f769becb07d523d79ad14da3
kevinluolog <kevinluolog_72@163.com>
2019-10-29 12:36:48 +0800

    kl-touch-file-time.js: 加入 hook:before_post_render, data.date = data.updated;

--- MakefileLinuxkblog.mk
+++ MakefileLinuxkblog.mk
@@ -1,6 +1,12 @@
 #linux专用
 # 为了解决git文件clone时间问题。想恢复到commit时间。
-# 由于git log 取回的是本机git保存的commit时间，所以在tavis CI上面没有意义了。但是经过这一个回合，训练了linux命令，tail,touch,date,sed,xargs,bash,while do,正则规则也加强记忆，makefile的eval2次展开机制理解更加清晰。
+# 由于git log 取回的是本机git保存的commit时间，所以要把repo的所有历史数据拉下来
+# 在tavis CI上，只要设置
+# git:
+#  depth: false
+# 在一般机器上clone,还不知道怎么实现，可能pull所有分支就可以了吧？？？
+# 经过这一个回合，训练了linux命令，tail,touch,date,sed,xargs,bash,while do,正则规则也加强记忆，makefile的eval2次展开机制理解更加清晰。
+
 # ###---输入变量
 
 # 基目录
